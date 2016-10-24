b0785f55a4db1dd21204f1eb3d0ea1aff47e2ceb
Alexey Suslov <alexei.suslov@gmail.com>
2016-10-24 23:31:41 +0300

    db query done

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,30 @@
 package controllers
 
-import play.api._
+import javax.inject.Inject
+
+import play.api.db.Database
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject()(db: Database) extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def maxPriceList(maxPrice: Double) = Action {
+    var outString = "Tour is "
+    val conn = db.getConnection()
+    try {
+      val stmt = conn.createStatement
+      val rs = stmt.executeQuery("SELECT * FROM TOUR WHERE PRICE < " + maxPrice)
+
+      while (rs.next()) {
+        outString += rs.getString("name")
+      }
+    } finally {
+      conn.close()
+    }
+    Ok(outString)
   }
 
 }
